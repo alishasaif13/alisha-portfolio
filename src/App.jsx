993484ef2469b07cf5cf69d761c63e71bc659323
alishasaif13 <alisha.saif456@gmail.com>
2025-10-13 07:38:51 -0700

    Fix case sensitivity for components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Navbar from "./Components/Navbar.jsx";
 
-import About from "./Components/About";
-import Education from "./Components/Education";
-import Skills from "./Components/Skills";
-import Experience from "./Components/Experience";
-import Footer from "./Components/Footer";
+import Navbar from "./components/Navbar";
+
+import About from "./components/About";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
+
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbar/>
       <About />
       <Education />
       <Skills />
